09c71a5bbb49328d43715f7f3df873878b367dbb
Chadwick Banning <walkamongus@users.noreply.github.com>
2015-07-02 17:18:11 -0400

    add tests for centrify::service class

---
+++ spec/classes/service_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe 'centrify', :type => :class do
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        let(:facts) do
+          facts
+        end
+
+        context "centrify::service class with default parameters" do
+          let(:params) {{ }}
+
+          it { is_expected.to contain_class('centrify::service') }
+          it { is_expected.to contain_service('centrifydc') }
+          it { is_expected.to contain_service('centrify-sshd') }
+        end
+      end
+    end
+  end
+end
