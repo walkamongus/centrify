3b9cbfd4a8874ccad34cec266d8a7f63bf8c9e1e
Chadwick Banning <walkamongus@users.noreply.github.com>
2015-07-02 17:38:22 -0400

    add file name, ownership, and mode params

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,12 +4,20 @@
 #
 class centrify::config {
 
-  file { $::centrify::dc_config_file:
+  file { 'centrifydc_config':
     ensure => present,
+    path   => $::centrify::dc_config_file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
   }
 
-  file { $::centrify::sshd_config_file:
+  file { 'centrifydc_sshd_config':
     ensure => present,
+    path   => $::centrify::sshd_config_file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0600',
   }
 
 }
