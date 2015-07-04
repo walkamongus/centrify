25447bf92b789439df58b8996133f4641c6c2a0c
Chadwick Banning <walkamongus@users.noreply.github.com>
2015-07-03 22:31:08 -0400

    add tests for keytab join files

--- spec/classes/adjoin__keytab_spec.rb
+++ spec/classes/adjoin__keytab_spec.rb
@@ -13,19 +13,39 @@ describe 'centrify' do
             :krb_ticket_join => 'true',
             :join_user       => 'user',
             :krb_keytab      => '/tmp/join.keytab',
+            :krb_config_file => '/etc/krb5.conf',
             :domain          => 'example.com',
           }}
 
           it { is_expected.to contain_class('centrify::adjoin::keytab') }
 
           it do
-            is_expected.to contain_exec('get_krb_ticket').with({
-              'path'        => '/usr/share/centrifydc/kerberos/bin:/usr/bin:/usr/sbin:/bin',
-              'command'     => 'kinit -kt /tmp/join.keytab user',
-            })
+            is_expected.to contain_file('krb_keytab').with({
+              'path'  => '/tmp/join.keytab',
+              'owner' => 'root',
+              'group' => 'root',
+              'mode'  => '0600',
+            }).that_comes_before('File[krb_configuration]')
           end
+
           it do
-            is_expected.to contain_exec('adjoin_with_keytab').with({
+            is_expected.to contain_file('krb_configuration').with({
+              'path'  => '/etc/krb5.conf',
+              'owner' => 'root',
+              'group' => 'root',
+              'mode'  => '0644',
+            }).that_comes_before('Exec[run_kinit_with_keytab]')
+          end
+
+          it do
+            is_expected.to contain_exec('run_kinit_with_keytab').with({
+              'path'    => '/usr/share/centrifydc/kerberos/bin:/usr/bin:/usr/sbin:/bin',
+              'command' => 'kinit -kt /tmp/join.keytab user',
+            }).that_comes_before('Exec[run_adjoin_with_keytab]')
+          end
+
+          it do
+            is_expected.to contain_exec('run_adjoin_with_keytab').with({
               'path'        => '/usr/bin:/usr/sbin:/bin',
               'command'     => 'adjoin --force -w example.com',
               'unless'      => 'adinfo -d | grep example.com',
