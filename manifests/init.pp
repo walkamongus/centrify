95c61ce78da45a1349a7948447e83ef2ddeb91bb
Chadwick Banning <walkamongus@users.noreply.github.com>
2015-07-06 15:24:27 -0400

    add params for control of contents of krb_config file

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,20 +8,22 @@
 #   Explanation of what this parameter affects and what it defaults to.
 #
 class centrify (
-  $dc_package_name     = $::centrify::params::dc_package_name,
-  $sshd_package_name   = $::centrify::params::sshd_package_name,
-  $dc_package_ensure   = $::centrify::params::dc_package_ensure,
-  $sshd_package_ensure = $::centrify::params::sshd_package_ensure,
-  $dc_service_name     = $::centrify::params::dc_service_name,
-  $sshd_service_name   = $::centrify::params::sshd_service_name,
-  $dc_config_file      = $::centrify::params::dc_config_file,
-  $sshd_config_file    = $::centrify::params::sshd_config_file,
-  $domain              = $::centrify::params::domain,
-  $join_user           = $::centrify::params::join_user,
-  $join_password       = $::centrify::params::join_password,
-  $krb_ticket_join     = $::centrify::params::krb_ticket_join,
-  $krb_keytab          = $::centrify::params::krb_keytab,
-  $krb_config_file     = $::centrify::params::krb_config_file,
+  $dc_package_name        = $::centrify::params::dc_package_name,
+  $sshd_package_name      = $::centrify::params::sshd_package_name,
+  $dc_package_ensure      = $::centrify::params::dc_package_ensure,
+  $sshd_package_ensure    = $::centrify::params::sshd_package_ensure,
+  $dc_service_name        = $::centrify::params::dc_service_name,
+  $sshd_service_name      = $::centrify::params::sshd_service_name,
+  $dc_config_file         = $::centrify::params::dc_config_file,
+  $sshd_config_file       = $::centrify::params::sshd_config_file,
+  $domain                 = $::centrify::params::domain,
+  $join_user              = $::centrify::params::join_user,
+  $join_password          = $::centrify::params::join_password,
+  $krb_ticket_join        = $::centrify::params::krb_ticket_join,
+  $krb_keytab             = $::centrify::params::krb_keytab,
+  $manage_krb_config_file = $::centrify::params::manage_krb_config_file,
+  $krb_config_file        = $::centrify::params::krb_config_file,
+  $krb_config             = $::centrify::params::krb_config,
 ) inherits ::centrify::params {
 
   # validate parameters here
