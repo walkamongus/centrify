84d320ce216e93065f719732eb3f661742e3573d
Chadwick Banning <walkamongus@users.noreply.github.com>
2015-07-07 09:27:35 -0400

    add tests for allowed and denied user files

--- spec/classes/config_spec.rb
+++ spec/classes/config_spec.rb
@@ -30,6 +30,72 @@ describe 'centrify' do
             })
           end
         end
+
+        context "centrify::config class with groups and users params" do
+          let(:params) {{
+            :allow_users  => [ 'good_user1', 'good_user2', ],
+            :deny_users   => [ 'bad_user', ],
+            :allow_groups => [ 'good_group', ],
+            :deny_groups  => [ 'bad_group1', 'bad_group2', ],
+          }}
+
+          it do
+            is_expected.to contain_file('allow_users_file').with({
+              'ensure' => 'present',
+              'path'   => '/etc/centrifydc/users.allow',
+              'owner'  => 'root',
+              'group'  => 'root',
+              'mode'   => '0644',
+            })
+          end
+
+          it { is_expected.to contain_file('allow_users_file').with_content(
+            /good_user1\\ngood_user2/
+          ) }
+
+          it do
+            is_expected.to contain_file('allow_groups_file').with({
+              'ensure' => 'present',
+              'path'   => '/etc/centrifydc/groups.allow',
+              'owner'  => 'root',
+              'group'  => 'root',
+              'mode'   => '0644',
+            })
+          end
+
+          it { is_expected.to contain_file('allow_groups_file').with_content(
+            /good_group/
+          ) }
+
+          it do
+            is_expected.to contain_file('deny_users_file').with({
+              'ensure' => 'present',
+              'path'   => '/etc/centrifydc/users.deny',
+              'owner'  => 'root',
+              'group'  => 'root',
+              'mode'   => '0644',
+            })
+          end
+
+          it { is_expected.to contain_file('deny_users_file').with_content(
+            /bad_user/
+          ) }
+
+          it do
+            is_expected.to contain_file('deny_groups_file').with({
+              'ensure' => 'present',
+              'path'   => '/etc/centrifydc/groups.deny',
+              'owner'  => 'root',
+              'group'  => 'root',
+              'mode'   => '0644',
+            })
+          end
+
+          it { is_expected.to contain_file('deny_groups_file').with_content(
+            /bad_group1\\nbad_group2/
+          ) }
+
+        end
       end
     end
   end
