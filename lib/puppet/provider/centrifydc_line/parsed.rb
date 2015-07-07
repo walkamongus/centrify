4324d318e0771e05254f602bc3266db7a7b962cb
Chadwick Banning <walkamongus@users.noreply.github.com>
2015-07-07 10:43:42 -0400

    add type and provider for managing lines in centrifydc.conf

---
+++ lib/puppet/provider/centrifydc_line/parsed.rb
@@ -0,0 +1,20 @@
+require 'puppet/provider/parsedfile'
+
+target = '/etc/centrifydc/centrifydc.conf'
+
+Puppet::Type.type(:centrifydc_line).provide(
+  :parsed,
+  :parent => Puppet::Provider::ParsedFile,
+  :default_target => target,
+  :filetype => :flat
+) do
+  desc 'The centrifydc_line provider that uses the ParsedFile class'
+  
+  text_line :comment, :match => /^\s*#/;
+
+  text_line :blank, :match => /^\s*$/;
+
+  record_line :parsed,
+    :fields => %w{setting value},
+    :separator => /\s*:\s*/
+end
---
+++ lib/puppet/type/centrifydc_line.rb
@@ -0,0 +1,29 @@
+Puppet::Type.newtype(:centrifydc_line) do
+  @doc = "Manage the contents of the centrifydc.conf file
+  centrifydc_line { 'log':
+    ensure => present,
+    value  => 'INFO',
+  }"
+
+  ensurable
+
+  newparam(:setting, :namevar => true) do
+    desc 'The configuration setting to manage'
+  end
+
+  newproperty(:value) do
+    desc 'The value for the configuration setting'
+    newvalues(/^[\.\w]+$/)
+  end
+
+  newproperty(:target) do
+    desc 'Location of the centrifydc.conf file'
+    defaultto {
+      if @resource.class.defaultprovider.ancestors.include? (Puppet::Provider::ParsedFile)
+        @resource.class.defaultprovider.default_target
+      else
+        nil
+      end
+    }
+  end
+end
