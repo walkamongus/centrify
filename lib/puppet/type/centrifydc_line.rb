da828003271449c9e48396bf3277ed7905d65c67
Chadwick Banning <walkamongus@users.noreply.github.com>
2015-07-08 09:29:50 -0400

    move validation to value param

--- lib/puppet/type/centrifydc_line.rb
+++ lib/puppet/type/centrifydc_line.rb
@@ -9,11 +9,11 @@ Puppet::Type.newtype(:centrifydc_line) do
 
   newparam(:setting, :namevar => true) do
     desc 'The configuration setting to manage'
-    newvalues(/^[\w\.]+$/)
   end
 
   newproperty(:value) do
     desc 'The value for the configuration setting'
+    newvalues(/^[\w\.:\/]+$/)
   end
 
   newproperty(:target) do
