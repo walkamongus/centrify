e1a29b0068e580b6e5ebfc04c12ada4f1a0a88ab
Chadwick Banning <walkamongus@users.noreply.github.com>
2015-07-10 08:30:54 -0400

    add adflush and adreload after password join and mark local variables

--- manifests/adjoin/password.pp
+++ manifests/adjoin/password.pp
@@ -5,15 +5,21 @@
 #
 class centrify::adjoin::password {
 
-  $user     = $::centrify::join_user
-  $password = $::centrify::join_password
-  $domain   = $::centrify::domain
+  $_user     = $::centrify::join_user
+  $_password = $::centrify::join_password
+  $_domain   = $::centrify::domain
 
   exec { 'adjoin_with_password':
     path        => '/usr/bin:/usr/sbin:/bin',
-    command     => "adjoin -w -u ${user} -p ${password} ${domain}",
-    unless      => "adinfo -d | grep ${domain}",
+    command     => "adjoin -w -u ${_user} -p ${_password} ${_domain}",
+    unless      => "adinfo -d | grep ${_domain}",
+    refreshonly => true,
+  }->
+  exec { 'run_adflush_and_adreload':
+    path        => '/usr/bin:/usr/sbin:/bin',
+    command     => 'adflush && adreload',
     refreshonly => true,
   }
+
 
 }
