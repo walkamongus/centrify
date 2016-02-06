6a76ecd0103a2b4894ad209342b927b6a7291fc3
onzyone <onzyone@gmail.com>
2016-02-06 15:05:09 -0500

    Update cron.pp

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -1,8 +1,9 @@
 class centrify::cron {
+  $_crontab_time_min         = $::crontab_time_min
   cron { 'flush-centrify-cache':
     ensure  =>  present,
     command =>  '/usr/sbin/adflush -f > /dev/null 2>&1',
     user    =>  root,
-    minute  =>  '*/30',
+    minute  =>  $_crontab_time_min,
   }
 }
